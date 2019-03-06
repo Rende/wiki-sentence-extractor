30f7bfed0166d203768035f1d957fa34a76d69a1
Aydan Rende <aydan.rende@dfki.de>
2019-03-06 11:46:09 +0100

    Reorganize config

--- src/main/java/de/dfki/mlt/wse/preferences/Config.java
+++ src/main/java/de/dfki/mlt/wse/preferences/Config.java
@@ -25,7 +25,7 @@ public final class Config {
 	public static final String WIKIPEDIA_EXTENSION = "wikipedia.extension";
 	public static final String NUMBER_OF_SHARDS = "number_of_shards";
 	public static final String NUMBER_OF_REPLICAS = "number_of_replicas";
-	public static final String WIKIPEDIA_INVALID_PAGES = "wikipedia.invalid.page";
+	public static final String WIKIPEDIA_INVALID_PAGES = "wikipedia.page.invalid";
 	public static final String PAGE_REDIRECT = "wikipedia.page.redirect";
 	public static final String LANG = "language";
 
